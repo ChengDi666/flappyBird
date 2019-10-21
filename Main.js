ac27591da334475765c166d37de7638a12004ba4
ChengDi <1234567@qq.com>
2019-10-21 10:43:52 +0800

    第二天
    模拟变量池，定义图片父类

--- Main.js
+++ Main.js
@@ -1,20 +1,39 @@
 //  这是程序的主类，用于小游戏的过程中数据的初始化，以及点击的绑定
 
 import { ResourceLoader } from "./js/base/ResourceLoader.js";
+import { DataStore } from "./js/base/DataStore.js";
 
 export class Main{
     constructor(){
         console.log('游戏开始了');
         //  初始化画布
-        this.cancas = document.getElementById('game');
-        this.ctx = this.cancas.getContext('2d');
+        this.canvas = document.getElementById('game');
+        this.ctx = this.canvas.getContext('2d');
         //  初始化资源加载器
         this.loader = new ResourceLoader();
+        //  初始化变量池
+        this.dataStore = DataStore.getInstance();
+
         //  加载完成后执行其他的操作
         this.loader.onloaded(map=> this.onResourceLoaded(map));
     }
     //  资源加载完成后执行，其他操作的方法
     onResourceLoaded(map){
         console.log(map);
+        //  模拟化背景图
+        let bg = map.get("background"); //  那背景图片
+        this.ctx.drawImage(bg,0,0,bg.width,bg.height,0,0,this.canvas.width,this.canvas.height);
+
+        //  保存各种资源
+        //  不使用set方法保存的原因：
+        //      set 保存的数据，在游戏结束会被销毁，下面的数据
+        //      游戏结束也不需要需要销毁，可以接着使用，所以用属性保存
+        this.dataStore.canvas = this.canvas;
+        this.dataStore.ctx = this.ctx;
+        this.dataStore.res = map;
+        this.init();
     }
+
+    //  游戏初始化
+    init(){}
 }
---
+++ js/base/Sprite.js
@@ -0,0 +1,31 @@
+//  各种图片的父类
+
+import { DataStore } from "./js/base/DataStore.js";
+
+export class Sprite{
+    //  图，图的横,纵坐标，图的宽,高，画的横,纵坐标,画的宽,高
+    constructor(img=null,srcX=0,srcY=0,srcW=0,srcH=0,x=0,y=0,width=0,height=0) {
+        //  获取变量池
+        const dataStore = DataStore.getInstance();
+        this.ctx = dataStore.ctx;
+        this.img = img;     //  图片
+        this.srcX = srcX;   //  图片的起始X坐标
+        this.srcY = srcY;   //  起始Y坐标
+        this.srcW = srcW;   //  图片的宽
+        this.srcH = srcH;   //  图片的高
+        this.x = x;     //  在画布上的X坐标
+        this.y = y;     //  在画布上的Y坐标
+        this.width = width;     //  画的宽度
+        this.height = height;   //  画的高度
+    }
+
+    //  画图
+    draw(img=this.img,srcX=this.srcX,srcY=this.srcY,srcW=this.srcW,srcH=this.srcH,x=this.x,y=this.y,width=this.width,height=this.height){
+        this.ctx.drawImage(img,srcX,srcY,srcW,srcH,x,y,width,height);
+    }
+
+    //  获取指定图片
+    static detImage(key){
+        return DataStore.getInstance().res.get(key);
+    }
+}
